db31c8d16f8662b39ceb6227d87e04e0ccc49af5
derui <derutakayu@gmail.com>
2021-09-04 10:13:37 +0900

    refactor: progressing to use dune...

--- src/realcaml.ml
+++ src/realcaml.ml
@@ -1,3 +1,6 @@
+module Util = Realcaml_util
+module Mesh = Realcaml_mesh
+module Contact = struct
 module Setup         = Realcaml_contact_setup
 module Closest_point = Realcaml_contact_closest_point
 module Constraint    = Realcaml_contact_constraint
@@ -5,3 +8,7 @@ module Contact       = Realcaml_contact_contact
 module Contact_point = Realcaml_contact_contact_point
 module Pair = Realcaml_contact_pair
 module Constraint_solver = Realcaml_contact_constraint_solver
+  end
+module Engine = Realcaml_engine
+module Rigid_body = Realcaml_rigid_body
+module Voronoi = Realcaml_voronoi
--- src/std.ml
+++
@@ -1,6 +0,0 @@
-module Util = Realcaml_util
-module Mesh = Realcaml_mesh
-module Contact = Realcaml_contact
-module Engine = Realcaml_engine
-module Rigid_body = Realcaml_rigid_body
-module Voronoi = Realcaml_voronoi
